3ace92e28c07b91f196660f1b6415c700d53b16b
unknown <talktogbenga@gmail.com>
2025-02-07 20:28:26 +0100

    update Bulk mesage

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/invitee/", // Change 'myapp' to your desired directory name
+  build: {
+    outDir: "dist/invitee", // Ensures built files are placed in 'dist/myapp'
+  },
+});
